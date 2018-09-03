52e4e2c7ee252e87318b5b1f3e06ff5d63f6a436
blar khalid <blarharki@gmail.com>
2018-09-03 09:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const currentUser = 'Blar khalid';
 const welcomeMessage='Welcome to Flatbook, '+currentUser+'!';
 const  excitedWelcomeMessage=welcomeMessage.toUpperCase();
-const shortGreeting ='Welcome, ';
+let shortGreeting ='Welcome, ';
  const firstInitial = currentUser[0];
  const restOfName = currentUser.slice(1);
-     const shortGreeting='Welcome, ' + firstInitial + '!';
+      shortGreeting='Welcome, ' + firstInitial + '!';

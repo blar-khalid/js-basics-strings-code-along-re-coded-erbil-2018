b5c9b99c608ef3af29acd9a49d767eec11f24a65
blar khalid <blarharki@gmail.com>
2018-09-03 09:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Blar khalid';
 const welcomeMessage='Welcome to Flatbook, '+currentUser+'!';
-const  excitedWelcomeMessage=welcomeMessage.Uppe
+const  excitedWelcomeMessage=welcomeMessage.toUppercase()
